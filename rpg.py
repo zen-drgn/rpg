db0389983545b716504c0a8997361d6f8b6c0791
kweli_debug <mystical.kg@outlook.com>
2025-09-08 18:02:01 -0600

    feat: add an attack and heal function
    a character or user can now deal attack damage based on their stats
    as well as heal a certain amount when damaged/attacked

--- rpg.py
+++ rpg.py
@@ -4,8 +4,8 @@ def create_character():
         "name": "Kweli",
         "class": "Sage",
         "health": 100,
-        "strength": 77,
-        "luck": 0.5
+        "strength": 10,
+        "luck": 0.8
     }
     return chara_dict
 
@@ -13,17 +13,30 @@ def print_character(character):
     print(f"Welcome to the Fantasy RPG Character Generator\nyour hero is named {character["name"]} the {character["class"]}\n") 
     print(f"Stats:\n\n- Health: {character["health"]}\n- Strength: {character["strength"]}\n- Luck: {character["luck"]}")
      
-
+#calculate the damage a character can deal based on stats like strength and luck
 def attack(character):
-    pass
+    base_damage = character["strength"] * 2
+    if character["luck"] > 0.5:
+        base_damage += 5
+    line = f"{character['name']} attacks and deals {base_damage} damage!"
+    return line 
 
-# bellow section to test run code
+#heal a character with an amount that adds to their total health 
+def heal(character, amount):
+    character["health"] += amount
+    line = f"{character['name']} has healed by {amount} points and now has {character['health']} health"
+    return line
+
+# bellow section to run code
 the_character = create_character()
 #print(the_character)
 
-print(print_character(the_character))
+print_character(the_character)
 
-attack(the_character)
+print(attack(the_character))
 
+print(heal(the_character, 30))
+
+print_character(the_character)
 
 
